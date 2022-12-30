36d33248b6cc5c14248f30eac06198cb69e93d0e
Felipe Rodrigues <lipe.h.r@gmail.com>
2022-12-30 14:15:50 -0300

    removed bg animation, added dark mode button and changed some styles

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 let currentYear = new Date().getFullYear();
-document.getElementById('year').innerHTML = currentYear;
+document.getElementById("year").innerHTML = currentYear;
 
-setInterval(function() {
-  if (currentYear !== document.getElementById('year').innerHTML) {
-    document.getElementById('year').innerHTML = "&copy " + currentYear + " - " + "Felipe Rodrigues";
+setInterval(function () {
+  if (currentYear !== document.getElementById("year").innerHTML) {
+    document.getElementById("year").innerHTML =
+      "&copy " + currentYear + " - " + "Felipe Rodrigues";
   }
 }, 1000);
+
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  const body = document.body;
+
+  if (html.classList.contains("light")) {
+    body.setAttribute("style", "background: var(--first-color)");
+  } else {
+    body.setAttribute("style", "background: var(--third-color)");
+  }
+}
