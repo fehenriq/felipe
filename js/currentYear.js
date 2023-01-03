7ed9a92674ed32fea85613ab6c6b206d1ef87282
Felipe Rodrigues <lipe.h.r@gmail.com>
2023-01-03 10:36:32 -0300

    fix and gap changes

---
+++ js/currentYear.js
@@ -0,0 +1,10 @@
+let currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
+setInterval(function () {
+  if (currentYear !== document.getElementById("year").innerHTML) {
+    document.getElementById("year").innerHTML =
+      "&copy " + currentYear + " - " + "Felipe Rodrigues";
+  }
+}, 1000);
+
--- js/toggleButton.js
+++ js/toggleButton.js
@@ -1,13 +1,3 @@
-let currentYear = new Date().getFullYear();
-document.getElementById("year").innerHTML = currentYear;
-
-setInterval(function () {
-  if (currentYear !== document.getElementById("year").innerHTML) {
-    document.getElementById("year").innerHTML =
-      "&copy " + currentYear + " - " + "Felipe Rodrigues";
-  }
-}, 1000);
-
 function toggleButton() {
   const html = document.documentElement;
   html.classList.toggle("light");
